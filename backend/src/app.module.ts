96e747a943048cd2b312e589149a80b475a554ec
thesalmankhxn <thesalmankhxn@gmail.com>
2025-05-12 05:18:16 +0530

    un-comment: app controller & service;

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
@@ -27,7 +27,7 @@ import { TodosModule } from './todos/todos.module';
     }),
     TodosModule,
   ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
