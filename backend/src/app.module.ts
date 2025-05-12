e6b7d9fa9767e16cffdb084ffcf638638ccf60e7
thesalmankhxn <thesalmankhxn@gmail.com>
2025-05-12 06:31:01 +0530

    remove: default naming string;

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
-import { DefaultNamingStrategy } from 'typeorm';
-
-class CustomNamingStrategy extends DefaultNamingStrategy {
-  generateString() {
-    return Math.random().toString(36).substring(2, 15);
-  }
-}
 
 @Module({
   imports: [
@@ -29,7 +22,6 @@ class CustomNamingStrategy extends DefaultNamingStrategy {
         database: configService.get('DATABASE_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // Set to false in production
-        namingStrategy: new CustomNamingStrategy(),
       }),
       inject: [ConfigService],
     }),
