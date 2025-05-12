93f95ac2c97f7177729fe727c4d8cd2f04003f7d
thesalmankhxn <thesalmankhxn@gmail.com>
2025-05-12 05:44:32 +0530

    fix: local env, import env file;

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +9,8 @@ async function bootstrap() {
   app.enableCors();
 
   // process.env.DATABASE_PORT ||
-  const port = 3000;
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
