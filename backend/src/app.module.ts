ffa3846c0a858f3d9ec64bb99d41e43b18d04f4d
thesalmankhxn <thesalmankhxn@gmail.com>
2025-05-12 06:27:36 +0530

    fix: deployments;

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { DefaultNamingStrategy } from 'typeorm';
+
+class CustomNamingStrategy extends DefaultNamingStrategy {
+  generateString() {
+    return Math.random().toString(36).substring(2, 15);
+  }
+}
 
 @Module({
   imports: [
@@ -22,6 +29,7 @@ import { TodosModule } from './todos/todos.module';
         database: configService.get('DATABASE_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // Set to false in production
+        namingStrategy: new CustomNamingStrategy(),
       }),
       inject: [ConfigService],
     }),
