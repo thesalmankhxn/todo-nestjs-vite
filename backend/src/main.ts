2711b369a7d6e9506377cb487320d6e4ed53487f
thesalmankhxn <thesalmankhxn@gmail.com>
2025-05-13 12:08:44 +0530

    fix: Modified the enableCors() call to include specific configuration options;

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://todo-nestjs-vite.vercel.app', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   // Global validation pipe
   app.useGlobalPipes(
